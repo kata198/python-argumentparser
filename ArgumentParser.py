02f5da6986e5a2e0463cafa4534dac891496245e
Tim Savannah <kata198@gmail.com>
2015-02-10 16:13:09 -0500

    Initial Commit

---
+++ ArgumentParser.py
@@ -0,0 +1,150 @@
+# Copyright 2015 (c) Tim Savannah
+# GPL License
+
+class ArgumentParser(object):
+	'''
+		A utility class for parsing command-line arguments.
+
+		@param names list<string> - This is a list of "names" which should reflect each argument.
+		@param shortOptions list<string>  - This is a list of short (-x val) options. Length should match names. If no short option is available, use 'None'. Omit the leading -
+		@param longOptions  list<string>  - This is a list of long (--xyz val or --xyz= val) options. If no long option is available, use 'None'. Omit the leading --
+		@param staticOptions list<string> - This is a list of static options (arguments that have meaning just being present, without taking an additional value).
+												Any members of this list will be present in the results of #parse, set to True is present, otherwise False.
+
+
+
+	    @example
+In [1]: from ArgumentParser import ArgumentParser
+
+In [2]: parser = ArgumentParser(['firstName', 'lastName', 'birthday'],
+   ...:     ['f', 'l', 'b'],
+   ...:     ['first-name', 'last-name', 'birthday'],
+   ...:     ['--citizen']
+   ...: )
+
+In [3]: parser.parse('-f Tim --last-name=savannah --birthday 6/28'.split(' '))
+Out[3]:
+{'errors': [],
+ 'result': {'--citizen': False,
+  'birthday': '6/28',
+  'firstName': 'Tim',
+  'lastName': 'savannah'},
+ 'warnings': []}
+
+In [4]: parser.parse('-f Tim --last-name=savannah --citizen'.split(' '))
+Out[4]:
+{'errors': [],
+ 'result': {'--citizen': True, 'firstName': 'Tim', 'lastName': 'savannah'},
+ 'warnings': []}
+
+	'''
+
+	def __init__(self, names, shortOptions, longOptions, staticOptions=None):
+		namesLen = len(names)
+		if namesLen != len(shortOptions) or namesLen != len(longOptions):
+			raise ValueError('names, shortOptions, longOptions must all have the same length. use None of there is no equivlant.')
+
+		self.names = names
+		self.shortOptions = shortOptions
+		self.longOptions = longOptions
+		self.staticOptions = staticOptions or []
+
+
+	def parse(self, args):
+		'''
+			parse - Parses provided arguments and returns information on them. If using sys.argv, omit the first argument.
+
+			@return - dict keys are
+						'result' => dictionary of result name->value
+						'errors'  => list of strings of errors or empty list
+						'warnings' => list of strings of warnings, or empty list
+		'''
+		result = {}
+		errors = []
+		warnings = []
+		
+		argsLen = len(args)
+		i = 0
+		while i < argsLen:
+			arg = args[i]
+			if arg in self.staticOptions:
+				if arg in result:
+					warnings.append("Argument '%s' specified more than once (at position %d)" %(arg, i))
+				else:
+					result[arg] = True
+				i += 1
+				continue
+			
+			if arg.startswith('--'):
+				try:
+					equalSignIdx = arg.index('=')
+					argName = arg[2:equalSignIdx]
+				except ValueError:
+					equalSignIdx = False
+					argName = arg[2:]
+				try:
+					optionIdx = self.longOptions.index(argName)
+				except ValueError:
+					errors.append("Unknown option at '%s' (at position %d)" %(arg, i,))
+					i += 1
+					continue
+				
+				name = self.names[optionIdx]
+				if name in result:
+					warnings.append("%s specified more than once. Using latter value (at position %d)" %(name, i))
+
+				if equalSignIdx is not False:
+					val = arg[equalSignIdx+1:]
+					if len(val) == 0:
+						errors.append('Equal sign used and no value on %s (at position %d)' %(arg, i))
+						i += 1
+						continue
+					result[name] = val
+					i += 1
+					continue
+				else:
+					if i+1 >= argsLen:
+						errors.append('No equal sign provided on assignment option, and no more arguments to try at %s (at position %d)' %(arg, i))
+						i += 2
+						continue
+					val = args[i+1]
+					result[name] = val
+					i += 2
+					continue
+
+			elif arg.startswith('-'):
+				argName = arg[1:]
+				if i+1 >= argsLen:
+					errors.append('No assignment on short option %s (at position %d)' %(arg, i))
+					i += 2
+					continue
+				
+				try:
+					optionIdx = self.shortOptions.index(argName)
+				except ValueError:
+					errors.append("Unknown option at position '%s' (at position %d)" %(arg, i))
+					i += 1
+					continue
+
+				name = self.names[optionIdx]
+				if name in result:
+					warnings.append("%s specified more than once, using latter value (at position %d)" %(name, i))
+
+				result[name] = args[i+1]
+				i += 2
+				continue
+			
+			else:
+				errors.append('Unknown argument %s (at position %d)' %(arg, i))
+				i += 1
+				continue
+
+		for staticOption in self.staticOptions:
+			if staticOption not in result:
+				result[staticOption] = False
+		
+		return { 'result' : result, 'errors' : errors, 'warnings' : warnings }
+
+				
+				
+# vim: sw=4 ts=4 noexpandtab
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # python-argumentparser
 A python2/3 compatible commandline argument parser.
+
+Usage
+=====
+
+A utility class for parsing command-line arguments.
+
+Constructor:
+
+@param names list<string> - This is a list of "names" which should reflect each argument.
+@param shortOptions list<string>  - This is a list of short (-x val) options. Length should match names. If no short option is available, use 'None'. Omit the leading -
+@param longOptions  list<string>  - This is a list of long (--xyz val or --xyz= val) options. If no long option is available, use 'None'. Omit the leading --
+@param staticOptions list<string> - This is a list of static options (arguments that have meaning just being present, without taking an additional value).
+                                                Any members of this list will be present in the results of #parse, set to True is present, otherwise False.
+
+Functions:
+def parse(args)
+	parse - Parses provided arguments and returns information on them. If using sys.argv, omit the first argument.
+
+	@return - dict keys are
+				'result' => dictionary of result name->value
+				'errors'  => list of strings of errors or empty list
+				'warnings' => list of strings of warnings, or empty list
+
+
+
+Example
+=======
+
+In [1]: from ArgumentParser import ArgumentParser
+
+In [2]: parser = ArgumentParser(['firstName', 'lastName', 'birthday'],
+   ...:     ['f', 'l', 'b'],
+   ...:     ['first-name', 'last-name', 'birthday'],
+   ...:     ['--citizen']
+   ...: )
+
+In [3]: parser.parse('-f Tim --last-name=savannah --birthday 6/28'.split(' '))
+Out[3]:
+{'errors': [],
+ 'result': {'--citizen': False,
+  'birthday': '6/28',
+  'firstName': 'Tim',
+  'lastName': 'savannah'},
+ 'warnings': []}
+
+In [4]: parser.parse('-f Tim --last-name=savannah --citizen'.split(' '))
+Out[4]:
+{'errors': [],
+ 'result': {'--citizen': True, 'firstName': 'Tim', 'lastName': 'savannah'},
+ 'warnings': []}
+
